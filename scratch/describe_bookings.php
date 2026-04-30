b90909172db417f2cf90d7718d2a80d5bc40d86d
Adhikari Sujal <sujaladhikari487@gmail.com>
2026-04-30 15:10:21 +0545

    updated schema

--- scratch/describe_bookings.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'app/config/database.php';
-$db = new Database();
-$pdo = $db->getConnection();
-$stmt = $pdo->prepare('DESCRIBE bookings');
-$stmt->execute();
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
