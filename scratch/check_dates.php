bf333f5c54695f8d12d84698b25c39da857a21f0
Sujal Adhikari <sujaladhikari487@gmail.com>
2026-05-17 01:00:13 +0545

    just a tiny change in button

--- scratch/check_dates.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'app/config/database.php';
-$db = (new Database())->getConnection();
-$sql = "SELECT b.event_date FROM bookings b JOIN events e ON b.event_id = e.id WHERE e.organizer_id = 1";
-$stmt = $db->query($sql);
-echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
